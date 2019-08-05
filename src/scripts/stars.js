fde236c7ae01ec58fd50102d67849b16df065c1d
Kamil Polanski <polanski88@gmail.com>
2019-08-05 21:26:02 +0200

    Modify stars.js

--- src/scripts/stars.js
+++ src/scripts/stars.js
@@ -1,20 +1,20 @@
 const rates = document.querySelectorAll('.stars'); // take all ratings
-rates.forEach(ele => {
-  ele.addEventListener('mouseenter', event => {
+rates.forEach(elem => {
+  elem.addEventListener('mouseenter', event => {
     event.preventDefault();
     // control for star-rating class
-    if (!ele.classList.contains('clicked')) {
+    if (!elem.classList.contains('clicked')) {
       // check for active class
-      ele.classList.add('stars-high');
+      elem.classList.add('stars-high');
     }
-    ele.addEventListener('click', () => {
+    elem.addEventListener('click', () => {
       // add active class on click
-      ele.classList.add('stars-high', 'clicked');
+      elem.classList.add('stars-high', 'clicked');
     });
-    ele.addEventListener('mouseleave', () => {
+    elem.addEventListener('mouseleave', () => {
       // remove active class
-      if (!ele.classList.contains('clicked')) {
-        ele.classList.remove('stars-high');
+      if (!elem.classList.contains('clicked')) {
+        elem.classList.remove('stars-high');
       }
     });
 
@@ -22,26 +22,26 @@ rates.forEach(ele => {
     let full = Array.from(event.target.querySelectorAll('.full')); // take amount of full stars from rating
     const stars = Array.from(event.target.querySelectorAll('span')); // take each star from rating
 
-    stars.forEach(ele => {
-      ele.addEventListener('click', () => {
+    stars.forEach(elem => {
+      elem.addEventListener('click', () => {
         // add or remove star on click
-        starControl(ele);
+        starControl(elem);
         full = Array.from(event.target.querySelectorAll('.full')); // update amount of full stars from rating
       });
-      ele.addEventListener('mouseenter', () => {
+      elem.addEventListener('mouseenter', () => {
         // add stars on hover
-        starControl(ele);
+        starControl(elem);
       });
-      ele.addEventListener('mouseleave', () => {
+      elem.addEventListener('mouseleave', () => {
         // remove stars after hover
-        const sibs = getPreviousSiblings(ele);
-        ele.classList.remove('full');
-        sibs.forEach(ele => {
-          ele.classList.remove('full');
+        const sibs = getPreviousSiblings(elem);
+        elem.classList.remove('full');
+        sibs.forEach(elem => {
+          elem.classList.remove('full');
         });
-        full.forEach(ele => {
+        full.forEach(elem => {
           // return amount of full stars before hover
-          ele.classList.add('full');
+          elem.classList.add('full');
         });
       });
     });
@@ -49,15 +49,15 @@ rates.forEach(ele => {
 });
 
 // function for add/remove stars
-const starControl = ele => {
-  const sibs = getPreviousSiblings(ele);
-  const nextSibling = getNextSiblings(ele);
-  ele.classList.add('full');
-  sibs.forEach(ele => {
-    ele.classList.add('full');
+const starControl = elem => {
+  const sibs = getPreviousSiblings(elem);
+  const nextSibling = getNextSiblings(elem);
+  elem.classList.add('full');
+  sibs.forEach(elem => {
+    elem.classList.add('full');
   });
-  nextSibling.forEach(ele => {
-    ele.classList.remove('full');
+  nextSibling.forEach(elem => {
+    elem.classList.remove('full');
   });
 };
 
@@ -76,8 +76,7 @@ const getNextSiblings = (elem, filter) => {
   const sibs = [];
   let nextElem = elem.parentNode.firstChild;
   do {
-    if (nextElem.nodeType === 3) continue; // ignore text nodes
-    if (nextElem === elem) continue; // ignore elem of target
+    if (nextElem.nodeType === 3 || nextElem === elem) continue; // ignore text nodes, ignore elem of target
     if (nextElem === elem.nextElementSibling) {
       if (!filter || filter(elem)) {
         sibs.push(nextElem);
