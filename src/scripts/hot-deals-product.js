4f26fe2eed112bb873bc67b54f7d3f2a02a6d224
Dzesika Tkaczyk <dzesika.tkaczyk@gmail.com>
2019-08-08 11:29:21 +0200

    add dots change

--- src/scripts/hot-deals-product.js
+++ src/scripts/hot-deals-product.js
@@ -12,12 +12,34 @@ function isActive () {
 
 function autoChange () {
   var i = isActive();
-  productBox[i].classList.remove('active-hot-deal');
+  for (var j = 0; j < productBoxLen; j++) {
+    productBox[j].classList.remove('active-hot-deal');
+    dots[j].classList.remove('active');
+  }
   i++;
   if (i >= productBoxLen) {
     i = 0;
   }
   productBox[i].classList.add('active-hot-deal');
+  dots[i].classList.add('active');
 }
 
 setInterval(autoChange, 3000);
+
+var dots = document.querySelectorAll('.hot-dot');
+
+var showHotDeal = function () {
+  for (var i = 0; i < productBoxLen; i++) {
+    if (this === dots[i]) {
+      productBox[i].classList.add('active-hot-deal');
+      dots[i].classList.add('active');
+    } else {
+      productBox[i].classList.remove('active-hot-deal');
+      dots[i].classList.remove('active');
+    }
+  }
+};
+
+for (var i = 0; i < dots.length; i++) {
+  dots[i].addEventListener('click', showHotDeal);
+}
