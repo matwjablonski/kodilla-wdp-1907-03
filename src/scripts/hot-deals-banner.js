5d4798fcfcb0f5504cc30b2803e23c21eb97bb14
Dzesika Tkaczyk <dzesika.tkaczyk@gmail.com>
2019-08-14 21:06:02 +0200

    change swipe

--- src/scripts/hot-deals-banner.js
+++ src/scripts/hot-deals-banner.js
@@ -2,6 +2,12 @@ const previous = document.getElementById('button-previous');
 const next = document.getElementById('button-next');
 const images = document.querySelectorAll('.advertisement-photo');
 const banner = document.getElementById('banner');
+const threshold = 75; // required min distance traveled to be considered swipe
+const allowedTime = 200; // maximum time allowed to travel that distance
+let startX;
+let dist;
+let elapsedTime;
+let startTime;
 
 const imagesLen = images.length;
 
@@ -24,7 +30,6 @@ function nextPicture () {
 }
 
 next.addEventListener('click', nextPicture);
-banner.addEventListener('touchmove', nextPicture);
 
 function prevPicture () {
   let i = isActive();
@@ -37,4 +42,42 @@ function prevPicture () {
 }
 
 previous.addEventListener('click', prevPicture);
-banner.addEventListener('touchend', prevPicture);
+
+function bannerSwipe (swipe) {
+  if (swipe) {
+    nextPicture();
+  } else {
+    prevPicture();
+  }
+}
+
+banner.addEventListener(
+  'touchstart',
+  function (event) {
+    const touch = event.changedTouches[0];
+    dist = 0;
+    startX = touch.pageX;
+    startTime = new Date().getTime();
+  },
+  false
+);
+
+banner.addEventListener(
+  'touchend',
+  function (event) {
+    let swipeDirection;
+    const touch = event.changedTouches[0];
+    dist = touch.pageX - startX;
+    elapsedTime = new Date().getTime() - startTime;
+
+    if (dist > 0 && dist > threshold && elapsedTime <= allowedTime) {
+      swipeDirection = true; // next picture
+      bannerSwipe(swipeDirection);
+    } else if (dist < 0 && Math.abs(dist) > threshold && elapsedTime <= allowedTime) {
+      swipeDirection = false; // previous picture
+      bannerSwipe(swipeDirection);
+    } else {
+    }
+  },
+  false
+);
